63d8d2323e1ea052eaf6a9030a07edc3365b4974
ricky_yap <aryatamayaputera@gmail.com>
2025-06-22 02:07:43 +0700

    undo vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      'import.meta.env': {
-        VITE_BASE_URL: JSON.stringify(env.VITE_BASE_URL),
-      }
-    }
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
 });
