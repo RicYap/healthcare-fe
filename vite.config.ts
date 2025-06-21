79cf0fc5a940149ad5c8fe4d5f0e362ab4a72b34
ricky_yap <aryatamayaputera@gmail.com>
2025-06-22 00:49:58 +0700

    config env for vercel host

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'import.meta.env': {
+        VITE_BASE_URL: JSON.stringify(env.VITE_BASE_URL),
+      }
+    }
+  };
 });
